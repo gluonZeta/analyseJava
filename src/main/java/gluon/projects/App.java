690fdc1e5cb3f794914c66d258bf2002e38b1044
Gluon <andrainat@gmail.com>
2024-05-08 08:09:25 +0200

    LogBack installation

--- src/main/java/gluon/projects/App.java
+++ src/main/java/gluon/projects/App.java
@@ -1,13 +1,13 @@
 package gluon.projects;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        logger.info("Hehehe");
     }
 }
