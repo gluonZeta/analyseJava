2c32074739f4718f7f6a9ad166059461fa5bb6eb
Gluon <andrainat@gmail.com>
2024-05-11 07:30:48 +0200

    Initialisation Trading elements

---
+++ src/main/java/gluon/projects/trading/Main.java
@@ -0,0 +1,19 @@
+package gluon.projects.trading;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        List<String> cryptoSymbols = Utilities.getCryptoSymbolWithFilter();
+        for(int i = 0; i < cryptoSymbols.size(); i++) {
+            logger.info(cryptoSymbols.get(i));
+        }
+    }
+
+}
